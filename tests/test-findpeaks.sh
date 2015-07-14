d6e695b181e6c32d11804ba50e1d2fbad4011d73
Jeremiah Lant <jeremiahlant@gmail.com>
2015-07-14 15:50:15 -0400

    added a more informative message to screen when running tests

--- tests/test-findpeaks.sh
+++ tests/test-findpeaks.sh
@@ -2,7 +2,10 @@
 
 test_3_args()
 {
-    echo "testing 3 arguments"
+    echo "testing 3 arguments - should print the following"
+    echo ""
+    echo "data/test-dv.txt"
+    echo "  2014-09-04      50"
     echo ""
     bash findpeaks.sh -1 -d data/test-dv.txt    
 }
