414dd2d88ee7c5e966d24a0c21c2f5db073775f3
Jeremiah Lant <jeremiahlant@gmail.com>
2015-07-14 15:58:00 -0400

    added sample test files; updated test file to be more verbose; added blank findpeaks.sh file

--- tests/test-findpeaks.sh
+++ tests/test-findpeaks.sh
@@ -2,12 +2,17 @@
 
 test_3_args_1_peak_1_daily_value_file()
 {
-    echo "testing 3 arguments - should print the following"
+    echo "Testing 3 arguments 1 peak 1 daily value file"
+    echo ""
+    echo "Expected:"
     echo ""
     echo "data/test-dv.txt"
     echo "  2014-09-04      50"
     echo ""
+    echo "Actual:"
     bash findpeaks.sh -1 -d data/test-dv.txt    
+    echo ""
+    echo "---"
 }
 
 # main
