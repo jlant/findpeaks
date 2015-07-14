aa7097d4afc0700db06c482115442a92423cf91a
Jeremiah Lant <jeremiahlant@gmail.com>
2015-07-14 17:27:11 -0400

    updated test file and added a test for outputing 3 peaks

--- tests/test-findpeaks.sh
+++ tests/test-findpeaks.sh
@@ -16,5 +16,24 @@ test_3_args_1_peak_1_daily_value_file()
     echo "---"
 }
 
+test_3_args_3_peaks_1_daily_value_file()
+{
+    echo "Testing 3 arguments 3 peaks 1 daily value file"
+    echo ""
+    echo "Expected:"
+    echo ""
+    echo "tests/test-dv.txt"
+    echo "2014-09-04      50"
+    echo "2014-09-03      40"
+    echo "2014-09-05      50"
+    echo ""
+    echo "Actual:"
+    echo ""
+    bash findpeaks.sh -3 -d tests/test-dv.txt  
+    echo ""
+    echo "---"
+}
+
 # main
 test_3_args_1_peak_1_daily_value_file
+test_3_args_3_peaks_1_daily_value_file
