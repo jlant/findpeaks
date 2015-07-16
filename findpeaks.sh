7b13864a03cabb4368004ac6235f16e732edb128
Jeremiah Lant <jeremiahlant@gmail.com>
2015-07-16 15:33:49 -0400

    added tests and corresponding code for 2 args and for 1 args

--- findpeaks.sh
+++ findpeaks.sh
@@ -1,6 +1,21 @@
-NUMPEAKS=$1
-TYPE=$2
-FILENAME=$3
 
+# check that the correct number of arguments were supplied
+if [ $# -eq 3 ]; then
+    echo "3 args!"
+    NUMPEAKS=$1
+    TYPE=$2
+    FILENAME=$3
+elif [ $# -eq 2 ]; then
+    echo "2 args"
+    NUMPEAKS="-1"
+    TYPE=$1
+    FILENAME=$2
+else
+    echo "Not enough args supplied!"
+    echo "Usage: findpeaks.sh [numpeaks] type files"
+    exit 1
+fi
+
+# find peaks by cutting the appropriate columns and reverse sorting on the parameter value column (not the datetime column)
 echo $FILENAME
 grep "^USGS" $FILENAME | cut -f 3-4 | sort -rn -k2 | head $NUMPEAKS
--- tests/test-findpeaks.sh
+++ tests/test-findpeaks.sh
@@ -34,6 +34,40 @@ test_3_args_3_peaks_1_daily_value_file()
     echo "---"
 }
 
+test_2_args_1_peak_1_daily_value_file()
+{
+    echo "Testing 2 arguments 1 peak 1 daily value file - should use default of 1 for number of peaks"
+    echo ""
+    echo "Expected:"
+    echo ""
+    echo "tests/test-dv.txt"
+    echo "2014-09-04      50"
+    echo ""
+    echo "Actual:"
+    echo ""
+    bash findpeaks.sh -d tests/test-dv.txt    
+    echo ""
+    echo "---"
+}
+
+test_1_args_echo_error()
+{
+    echo "Testing 1 arguments 1 peak 1 daily value file - should use default of 1 for number of peaks"
+    echo ""
+    echo "Expected:"
+    echo ""
+    echo "Not enough args supplied!"
+    echo "Usage: findpeaks.sh [numpeaks] type files"
+    echo ""
+    echo "Actual:"
+    echo ""
+    bash findpeaks.sh tests/test-dv.txt    
+    echo ""
+    echo "---"
+}
+
 # main
 test_3_args_1_peak_1_daily_value_file
 test_3_args_3_peaks_1_daily_value_file
+test_2_args_1_peak_1_daily_value_file
+test_1_args_echo_error
