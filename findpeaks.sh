7163fdaf56d2e5c23ac76b789703e30dde8da442
Jeremiah Lant <jeremiahlant@gmail.com>
2015-07-21 12:40:36 -0400

    created a usage function to print usage and example; added a descriptive header

--- findpeaks.sh
+++ findpeaks.sh
@@ -1,25 +1,52 @@
 #!/bin/bash
 
-# check that the correct number of arguments were supplied
+# Name:
+#   findpeaks.sh - finds the largest (peak) value in USGS NWIS (http://waterdata.usgs.gov/nwis) data files
+#
+# Usage:
+#   findpeaks.sh [numpeaks] FILETYPE FILES
+#
+# Parameters:
+#   FILETYPE    [[-d | --daily ] | [-u | --unit]]  
+#   FILES       list of data file(s) to process
+#
+# Options:
+#   numpeaks    number of peaks to find 
+#
+# Description:
+#   The findpeaks.sh script finds the largest (peak) value in USGS NWIS (http://waterdata.usgs.gov/nwis) timeseries data files.
+#   The data files from the USGS NWIS are either daily timeseries (1 observation per day) or unit timeseries (96 observations
+#   per day 15 minute data) 
+#
+# Example:
+#   findpeaks.sh -2 -d sample-daily-value-file.txt 
+
+# function to display usage of program
+usage(){
+    echo "Usage: findpeaks.sh [numpeaks] FILETYPE FILES"
+    echo "Example: findpeaks.sh -2 -d sample-daily-value-file.txt"    
+}
+
+# check number of arguments; print error and usage if the incorrect number of arguments are entered, otherwise assign appropriately
 if [ $# -eq 3 ]; then
     NUMPEAKS=$1
-    TYPE=$2
+    FILETYPE=$2
     FILENAME=$3
 elif [ $# -eq 2 ]; then
     NUMPEAKS="-1"
-    TYPE=$1
+    FILETYPE=$1
     FILENAME=$2
 else
     echo "Incorrect number of args supplied!"
-    echo "Usage: findpeaks.sh [numpeaks] type files"
-    echo "Example: findpeaks.sh -2 -d sample-daily-value-file.txt"
+    echo 
+    usage
     exit
 fi
 
 # find peaks by cutting the appropriate columns and reverse sorting on the parameter value column (not the datetime column)
 echo $FILENAME
-if [ "$TYPE" = "-d" ]; then
+if [ "$FILETYPE" = "-d" ]; then
     grep "^USGS" $FILENAME | cut -f 3-4 | sort -rn -k2 | head $NUMPEAKS
-elif [ "$TYPE" = "-u" ]; then
+elif [ "$FILETYPE" = "-u" ]; then
     grep "^USGS" $FILENAME | cut -f 3-5 | sort -rn -k4 | head $NUMPEAKS
 fi
--- tests/test-findpeaks.sh
+++ tests/test-findpeaks.sh
@@ -3,132 +3,135 @@
 test_3_args_1_peak_1_daily_value_file()
 {
     echo "Testing 3 arguments 1 peak 1 daily value file"
-    echo ""
+    echo 
     echo "Expected:"
-    echo ""
+    echo 
     echo "tests/test-dv.txt"
     echo "2014-09-04      50"
-    echo ""
+    echo 
     echo "Actual:"
-    echo ""
+    echo 
     bash findpeaks.sh -1 -d tests/test-dv.txt    
-    echo ""
+    echo 
     echo "---"
 }
 
 test_3_args_3_peaks_1_daily_value_file()
 {
     echo "Testing 3 arguments 3 peaks 1 daily value file"
-    echo ""
+    echo 
     echo "Expected:"
-    echo ""
+    echo 
     echo "tests/test-dv.txt"
     echo "2014-09-04      50"
     echo "2014-09-03      40"
     echo "2014-09-05      50"
-    echo ""
+    echo 
     echo "Actual:"
-    echo ""
+    echo 
     bash findpeaks.sh -3 -d tests/test-dv.txt  
-    echo ""
+    echo 
     echo "---"
 }
 
 test_2_args_1_peak_1_daily_value_file()
 {
     echo "Testing 2 arguments 1 peak 1 daily value file - should use default of 1 for number of peaks"
-    echo ""
+    echo 
     echo "Expected:"
-    echo ""
+    echo 
     echo "tests/test-dv.txt"
     echo "2014-09-04      50"
-    echo ""
+    echo 
     echo "Actual:"
-    echo ""
+    echo 
     bash findpeaks.sh -d tests/test-dv.txt    
-    echo ""
+    echo 
     echo "---"
 }
 
 test_3_args_1_peak_1_unit_value_file()
 {
     echo "Testing 3 arguments 1 peak 1 unit value file"
-    echo ""
+    echo 
     echo "Expected:"
-    echo ""
+    echo 
     echo "tests/test-uv.txt"
     echo "2015-03-16 00:30        EDT     20"
-    echo ""
+    echo 
     echo "Actual:"
-    echo ""
+    echo 
     bash findpeaks.sh -1 -u tests/test-uv.txt    
-    echo ""
+    echo 
     echo "---"
 }
 
 test_3_args_2_peaks_1_unit_value_file()
 {
     echo "Testing 3 arguments 2 peaks 1 unit value file"
-    echo ""
+    echo 
     echo "Expected:"
-    echo ""
+    echo 
     echo "tests/test-uv.txt"
     echo "2015-03-16 00:30        EDT     20"
     echo "2015-03-16 00:45        EDT     15"
-    echo ""
+    echo 
     echo "Actual:"
-    echo ""
+    echo 
     bash findpeaks.sh -2 -u tests/test-uv.txt  
-    echo ""
+    echo 
     echo "---"
 }
 
 test_2_args_1_peak_1_unit_value_file()
 {
     echo "Testing 2 arguments 1 peak 1 unit value file - should use default of 1 for number of peaks"
-    echo ""
+    echo 
     echo "Expected:"
-    echo ""
+    echo 
     echo "tests/test-uv.txt"
     echo "2015-03-16 00:30        EDT     20"
-    echo ""
+    echo 
     echo "Actual:"
-    echo ""
+    echo 
     bash findpeaks.sh -u tests/test-uv.txt    
-    echo ""
+    echo 
     echo "---"
 }
 
 test_1_args_echo_error_daily_value_file()
 {
     echo "Testing 1 arguments 1 peak 1 daily value file - should use default of 1 for number of peaks"
-    echo ""
+    echo 
     echo "Expected:"
-    echo ""
+    echo 
     echo "Incorrect number of args supplied!"
+    echo 
     echo "Usage: findpeaks.sh [numpeaks] type files"
-    echo ""
+    echo "Example: findpeaks.sh -2 -d sample-daily-value-file.txt"
+    echo 
     echo "Actual:"
-    echo ""
+    echo 
     bash findpeaks.sh tests/test-dv.txt    
-    echo ""
+    echo 
     echo "---"
 }
 
 test_1_args_echo_error_unit_value_file()
 {
     echo "Testing 1 arguments 1 peak 1 unit value file - should use default of 1 for number of peaks"
-    echo ""
+    echo 
     echo "Expected:"
-    echo ""
+    echo 
     echo "Incorrect number of args supplied!"
+    echo
     echo "Usage: findpeaks.sh [numpeaks] type files"
     echo "Example: findpeaks.sh -2 -d sample-daily-value-file.txt"
-    echo ""
+    echo 
     echo "Actual:"
-    echo ""
+    echo 
     bash findpeaks.sh tests/test-uv.txt    
-    echo ""
+    echo 
     echo "---"
 }
 
